f629b0b99d399a7adb82a631e90285a3557de98c
jadergermano <jader.germano@icloud.com>
2020-06-22 12:29:26 -0300

    Initial configurations

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+const PORT = 3333;
+
+app.use(express.json())
+app.use(routes);
+
+app.listen(3333, () =>{
+    console.log(`Server started on port ${PORT}`)
+})
